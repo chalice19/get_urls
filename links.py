d2a6c5e4347efdcc8486056f950fd4ea129d9cb4
Alexandra <aspasparagus@gmail.com>
2025-07-02 01:47:24 +0200

    Update links.py

--- links.py
+++ links.py
@@ -7,6 +7,7 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
+
 def only_links(href):
     return href and not href.startswith(('mailto:', 'tel:', 'sms:', 'javascript:'))
 
@@ -21,27 +22,24 @@ def split_link(url):
     third_slash = link.find('/', 8)
     if third_slash != -1:
         return link[:third_slash], link[third_slash:]
-    return link, '/'
+    return link.rstrip('/'), '/'
 
 parser = argparse.ArgumentParser(description='Find links on the given pages')
-parser.add_argument('-u', '--url', action='append', help='I will happily eat a url link provided here', )
+parser.add_argument('-u', '--url', action='append', help='I will happily eat a url link provided here', required=True)
 parser.add_argument('-o', '--output', help='In which format do you want me to give you the result?',
                     choices=['stdout', 'json'], default='stdout')
 
 args = parser.parse_args()
 use_json = args.output == 'json'
 input_urls = args.url
-if not input_urls:
-    print('No url given -> no links found. Please use -u option to provide some url')
-    exit()
 
 output_urls = {}
 for url in input_urls:
+    url = url.rstrip('/')
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     for a_tag in soup.find_all('a', href=only_links):
         link = a_tag.get('href')
-
         if absolute_link(link):
             if link.startswith('//'):
                 link = add_scheme(link, url)
@@ -49,7 +47,7 @@ for url in input_urls:
             base, relative = split_link(link)
             output_urls[base] = output_urls.get(base, []) + [relative]
         else:
-            output_urls[url] = output_urls.get(url, []) + [link]
+            output_urls[url.rstrip('/')] = output_urls.get(url, []) + [link]
 
 if use_json:
     js = json.dumps(output_urls, indent=2)
