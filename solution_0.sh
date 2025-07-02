0458738503be554e573f4682ba4995dfe196a74c
Alexandra <aspasparagus@gmail.com>
2025-07-02 17:11:21 +0200

    added comments

--- solution_0.sh
+++ solution_0.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
 
+# cast letter to lower case
+# remove http[s]:// from the beginning of the strings
+# remove trailing point
+# keep the last two part splitted by points
+# sort keeping the unque entries
 
 cat source.txt | \
   tr '[:upper:]' '[:lower:]' | \
-  sed 's~^[a-zA-Z]*://~~' | \
-  sed 's/[[:space:].]$//' | \
-  awk -F. '{ n = NF if (n >= 2) print $(n-1) "." $n }' | \
+  sed 's/^https\?:\/\///' | \
+  sed 's/[.]*$//'
+  awk -F. '{ n = NF ; print $(n-1) "." $n }' | \
   sort -u
--- solution_1.sh
+++ solution_1.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
 
+# cast letters to lower case
+# remove trailing dots
+# reverse the strings
+# split by / and keep only the first part
+# split by point and keep the two first parts
+# reverse back
+# sort
+# and keep only the unique entries
+
 cat source.txt | tr [A-Z] [a-z] | sed 's/\.$//' | rev | awk -F/ '{print $1}' | awk -F. '{printf "%s.%s\n", $1, $2}' | rev | sort | uniq
